18bdd5e793ed8d5c0130f606ebdd545885f5aa3f
Vitalii Shved <vitalyashved@gmail.com>
2023-03-06 16:19:32 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { increaseCount, decreaseCount } from "./redux/actions/actionCreator";
 
-function App() {
+const App = () => {
+  const count = useSelector((store) => store.counter.count);
+  // console.log(store);
+
+  const dispatch = useDispatch();
+
+  const handleIncrease = () => {
+    dispatch(increaseCount());
+  };
+
+  const handleDecrease = () => {
+    dispatch(decreaseCount());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleIncrease}>+1</button>
+      <button onClick={handleDecrease}>-1</button>
+      <h1>{count}</h1>
+    </>
   );
-}
+};
 
 export default App;
